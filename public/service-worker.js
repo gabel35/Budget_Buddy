eef2da418ccb256d54f51c373e5ebb4d57a39bda
gabel35 <gabrieljose3135@gmail.com>
2021-03-21 23:03:53 -0400

    added files to cache in service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/dist/app.bundle.js',
+    '/dist/favorites.bundle.js',
+    '/dist/topic.bundle.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+  ];
