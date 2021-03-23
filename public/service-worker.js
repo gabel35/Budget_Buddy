7c74386e9e498f97942f8c42ac3ee5ec88a4ad14
gabel35 <gabrieljose3135@gmail.com>
2021-03-23 01:58:34 -0400

    fixed service worker! missed an s...

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/style.css',
+    '/styles.css',
     '/dist/main.bundle.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
